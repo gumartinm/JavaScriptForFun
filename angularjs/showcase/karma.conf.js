81a834ac57d152267228a6510666a8992dbceb81
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2015-08-16 23:03:23 +0200

    showcase: karma.conf.js code coverages reports

--- angularjs/showcase/karma.conf.js
+++ angularjs/showcase/karma.conf.js
@@ -72,6 +72,23 @@ module.exports = function(config) {
       // suite: ''
     },
 
+    coverageReporter: {
+      // specify a common output directory
+      dir: 'test-tmp/coverage',
+      reporters: [
+        // reporters not supporting the `file` property
+        { type: 'html', subdir: 'report-html' },
+        { type: 'lcov', subdir: 'report-lcov' },
+        // reporters supporting the `file` property, use `subdir` to directly
+        // output them in the `dir` directory
+        { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
+        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+        { type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
+        { type: 'text', subdir: '.', file: 'text.txt' },
+        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+      ]
+    },
+
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
     singleRun: false,
