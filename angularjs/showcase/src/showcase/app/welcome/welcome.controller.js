dba91de34b4bf9d2f0127e26e3a12525204e48e2
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2015-08-20 02:19:18 +0200

    showcase: Manually Identify Dependencies with ng-strict-di vs @ngInject

--- angularjs/showcase/src/showcase/app/welcome/welcome.controller.js
+++ angularjs/showcase/src/showcase/app/welcome/welcome.controller.js
@@ -5,8 +5,13 @@
     .module('app.welcome')
     .controller('Welcome', Welcome);
 
+  // When using <div ng-app="app" ng-strict-di> (strict mode) we must always
+  // manually identify dependencies.
+  // Instead I am going to use ngInject because it is cool :)
+  // Welcome.$inject = ['$location'];
+
   /* @ngInject */
-  function Welcome() {
+  function Welcome($location) {
     var vm = this;
 
     vm.hello = 'Hello World';
