195ac9b0e2d13dda0c376a619f57e3b6ad6ebfc9
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2015-08-30 01:18:58 +0200

    showcase: cars.controller.spec creating spy without object

--- angularjs/showcase/src/showcase/app/cars/cars.controller.spec.js
+++ angularjs/showcase/src/showcase/app/cars/cars.controller.spec.js
@@ -2,6 +2,7 @@ describe('app.cars', function() {
   'use strict';
 
   var Cars;
+  // Why the heck do I need this stupid object if it is going to be spied by means of Jasmine?
   var cars = {
     getAll: function() {
       return {};
@@ -12,9 +13,10 @@ describe('app.cars', function() {
   beforeEach(function() {
     module('app.cars');
 
-    inject(function($controller, $location, _$q_) {
+    inject(function($controller, $modal, $timeout, _$q_) {
       Cars = $controller('Cars', {
-        $location: $location,
+        $modal: $modal,
+        $timeout: $timeout,
         cars: cars
       });
       $q = _$q_;
@@ -23,13 +25,52 @@ describe('app.cars', function() {
 
   describe('Cars controller', function () {
 
-    it('should invoke GET all cars in service', function () {
+    it('should invoke GET all cars in service: old fashionable way', function () {
 
       spyOn(cars, 'getAll')
         .and.callFake(function() {
           var deferred = $q.defer();
           return deferred.promise;
         });
+
+      Cars.getCars();
+
+      expect(cars.getAll).toHaveBeenCalled();
+    });
+  });
+
+});
+
+describe('app.cars', function() {
+  'use strict';
+
+  var Cars;
+  // With object already implementing the required spy :)
+  var cars = {
+    getAll: jasmine.createSpy('cars.getAll').and.callFake(function() {
+      return $q(function(resolve) {
+        resolve();
+      });
+    })
+  };
+  var $q;
+
+  beforeEach(function() {
+    module('app.cars');
+
+    inject(function($controller, $modal, $timeout, _$q_) {
+      Cars = $controller('Cars', {
+        $modal: $modal,
+        $timeout: $timeout,
+        cars: cars
+      });
+      $q = _$q_;
+    });
+  });
+
+  describe('Cars controller', function () {
+
+    it('should invoke GET all cars in service: alternative way', function () {
 
       Cars.getCars();
 
